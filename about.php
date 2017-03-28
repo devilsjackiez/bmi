50464355afd493c0265b555e528e82dc80c9c509
Jackrin Patanasutakit <j.patanasutakit@clbs.co.th>
2017-03-28 15:43:33 +0700

    Edit Navbar.

--- about.php
+++ about.php
@@ -50,7 +50,7 @@
                 <span class="icon-bar"></span>
             </button>
             <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
-            <a class="navbar-brand" href="index.php">Business Casual</a>
+            <a class="navbar-brand" href="index.php">ชุมชนคนรักสุขภาพ</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
